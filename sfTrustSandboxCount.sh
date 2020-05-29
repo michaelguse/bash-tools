41f9c21654e4524231f1831b01654df27b73d21b
Michael Guse <michael#optimizeconsulting.com>
2020-05-29 10:48:35 -0500

    Added compare logic for list of active sandboxes from Trust

--- sfTrustSandboxCount.sh
+++ sfTrustSandboxCount.sh
@@ -2,7 +2,17 @@
 
 echo "List of active sandboxes from Salesforce Trust:"
 echo 
-echo "Count#, Instance, Release Version, Location, Status"
+echo "Instance,Release Version,Location,Status,Count"
 
-curl -sS https://api.status.salesforce.com/v1/instances/ | jq -r -f countAllInst.jq | nl -ba -s ','
+if test -f sfTrustResult.new; then
+  mv sfTrustResult.new sfTrustResult.old
+fi
+
+curl -sS https://api.status.salesforce.com/v1/instances/ | jq -r -f countAllInst.jq | awk '{printf("%s,%d\n", $0, NR)}' > sfTrustResult.new
+
+cat sfTrustResult.new
+
+echo
+echo "Difference between current and last run results:"
+diff sfTrustResult.new sfTrustResult.old
 
