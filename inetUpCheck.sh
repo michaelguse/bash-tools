fdd699e8a04284b21040f3ed81a4111033ee8fad
Michael Guse <michael#optimizeconsulting.com>
2020-06-19 12:26:09 -0500

    Added inetCheck script

---
+++ inetUpCheck.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+INTERNET_STATUS="UNKNOWN"
+TIMESTAMP=`date +%s`
+STARTDOWN=TIMESTAMP
+STARTUP=TIMESTAMP
+while [ 1 ]
+ do
+    ping -c 1 -W 0.7 8.8.4.4 > /dev/null 2>&1
+    if [ $? -eq 0 ] ; then
+        if [ "$INTERNET_STATUS" != "UP" ]; then
+            STARTUP=`date +%s`
+            echo "UP   `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP)) $((`date +%s`-$STARTDOWN))";
+            INTERNET_STATUS="UP"
+        fi
+    else
+        if [ "$INTERNET_STATUS" = "UP" ]; then
+            STARTDOWN=`date +%s`
+            echo "DOWN `date +%Y-%m-%dT%H:%M:%S%Z` $((`date +%s`-$TIMESTAMP)) $((`date +%s`-$STARTUP))";
+            INTERNET_STATUS="DOWN"
+        fi
+    fi
+    sleep 1
+ done;
--- sfTrustSandboxCount.sh
+++ sfTrustSandboxCount.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 clear
 
-echo "List of active sandboxes from Salesforce Trust:"
-echo 
-echo "Instance,Release Version,Location,Status,Count"
-
 if test -f sfTrustResult.new; then
   mv sfTrustResult.new sfTrustResult.old
 fi
 
 curl -sS https://api.status.salesforce.com/v1/instances/ | jq -r -f countAllInst.jq | awk '{printf("%s,%d\n", $0, NR)}' > sfTrustResult.new
 
-cat sfTrustResult.new
+echo
+if test -s sfTrustResult.new; then
+  echo "Number of active sandbox Instances: " && wc -l sfTrustResult.new | awk '{print $1}'
+fi
 
-diff sfTrustResult.new sfTrustResult.old > diffResult.txt
+diff -y --left-column sfTrustResult.new sfTrustResult.old > diffResult.txt
 
 echo
 if test -s diffResult.txt; then
-  echo "Difference between current and last run results:"
+  echo "Instance,Release Version,Location,Status,Count"
   cat diffResult.txt
 else
   echo "No changes detected!"
 fi
+
 echo
 
