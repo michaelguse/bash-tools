a861ccf14adeb010ad1af28a453a52df756ade97
Michael Guse <michael#optimizeconsulting.com>
2020-08-20 13:37:08 -0500

    Add changes for app.js to exec scripts

---
+++ app.js
@@ -0,0 +1,10 @@
+const exec = require('child_process').exec;
+
+exec('sfTrustSandboxCount.sh', (error, stdout, stderr) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+  console.log(`stdout: ${stdout}`);
+  console.log(`stderr: ${stderr}`);
+});
