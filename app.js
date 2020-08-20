606e76361b8c05ed39328fc3d400b84e885e6a76
Michael Guse <michael#optimizeconsulting.com>
2020-08-20 13:43:57 -0500

    Changed procfile to worker process

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-exec('sfTrustSandboxCount.sh', (error, stdout, stderr) => {
+exec('./sfTrustSandboxCount.sh', (error, stdout, stderr) => {
   if (error) {
     console.error(`exec error: ${error}`);
     return;
