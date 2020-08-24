20661ebf5b48f453da2f97ab511a42894779eea6
Michael Guse <michael#optimizeconsulting.com>
2020-08-24 12:34:17 -0500

    Improved output/format for script

--- sfTrustSandboxCount.sh
+++ sfTrustSandboxCount.sh
@@ -8,6 +8,8 @@ if test -f sfTrustResult.new; then
   fi
 fi
 
+date "+%Y-%m-%d %H:%M:%S %Z" > newDate
+
 CURR_REL=$'Summer \'20'
 NEXT_REL=$'Winter \'21'
 
@@ -19,8 +21,11 @@ echo "|==                                                                  ==|"
 echo "|==   Current Release: ${CURR_REL}                                    ==|"
 echo "|==      Next Release: ${NEXT_REL}                                    ==|"
 echo "|==                                                                  ==|"
-
-date "+%Y-%m-%d %H:%M:%S %Z" > newDate
+echo "|==   Current run: `cat newDate`                           ==|"
+echo "|==      Last run: `cat oldDate`                           ==|"
+echo "|==                                                                  ==|"
+echo "|======================================================================|"
+echo
 
 curl -sS https://api.status.salesforce.com/v1/instances/ -o tmpFile
 
@@ -31,11 +36,7 @@ Instance,Release Version,Location,Status,Count
 ' <sfTrustResult.new >listOfActiveSandboxes.csv
 
 if test -s sfTrustResult.new; then
-  echo "|==   Current run: `cat newDate`                           ==|"
-  echo "|==      Last run: `cat oldDate`                           ==|"
-  echo "|==                                                                  ==|"
-  echo "|======================================================================|"
-  echo
+
   echo "Active Sandbox Statistics:"
   echo "  # of ${CURR_REL} sandboxes: `less sfTrustResult.new | grep -F "${CURR_REL}" | wc -l`"
   echo "  # of ${NEXT_REL} sandboxes: `less sfTrustResult.new | grep -F "${NEXT_REL}" | wc -l`"
