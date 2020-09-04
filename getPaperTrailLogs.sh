c3c19f2058502209d3489fd4606a0c28023fb51c
Michael Guse <michael#optimizeconsulting.com>
2020-09-04 09:38:12 -0500

    Only date updates

--- getPaperTrailLogs.sh
+++ getPaperTrailLogs.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-curl -sH 'X-Papertrail-Token: XPayAjsRI5kbOuueHdV' https://papertrailapp.com/api/v1/archives.json | \
+curl -sH 'X-Papertrail-Token: VxaeIfIpA0WtPgpzVkk' https://papertrailapp.com/api/v1/archives.json | \
 grep -o '"filename":"[^"]*"' | egrep -o '[0-9-]+' | \
-awk '$0 >= "2020-01-01" && $0 < "2020-09-03" {
+awk '$0 >= "2020-09-03" && $0 < "2020-09-04" {
   print "output " $0 ".tsv.gz"
   print "url https://papertrailapp.com/api/v1/archives/" $0 "/download"
-}' | \
-curl --progress-bar -fLH 'X-Papertrail-Token: XPayAjsRI5kbOuueHdV' -K-
+}' | curl --progress-bar -fLH 'X-Papertrail-Token: VxaeIfIpA0WtPgpzVkk' -K-
