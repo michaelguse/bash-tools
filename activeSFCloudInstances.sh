1d57295ed8156c775389fe94704c4a439eee4e08
Michael Guse <michael#optimizeconsulting.com>
2020-09-10 11:43:15 -0500

    Changed to display both RPOD and SB instances

--- activeSFCloudInstances.sh
+++ activeSFCloudInstances.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]] ; then
-    echo
-    echo "This command requires one argument."
-    echo 
-    echo "Usage:   $0 production|sandbox"
-    echo
-    exit 1
-fi
+PROD='production'
+SB='sandbox'
 
 curl -sS https://api.status.salesforce.com/v1/instances/ -o tmpFile
 
-jq -r --arg envType "$1" -f activeCountByEnvType.jq tmpFile > activeInst.log
+jq -r --arg envType "$PROD" -f activeCountByEnvType.jq tmpFile > activeInst.log
 
-printf "\nCount of active [$1] Cloud Instances:\n\n"
+printf "\nCount of active [$PROD] Cloud Instances:\n\n"
+
+len=`jq '. | length' activeInst.log` 
+
+for ((i=0; i<len; i++)); do
+    printf "\t`jq -r --arg ij "$i" .[' $ij|tonumber '].Count activeInst.log`\t- `jq -r --arg ij "$i" .[' $ij|tonumber '].ProductCloud activeInst.log`\n"
+done
+
+echo
+
+jq -r --arg envType "$SB" -f activeCountByEnvType.jq tmpFile > activeInst.log
+
+printf "\nCount of active [$SB] Cloud Instances:\n\n"
 
 len=`jq '. | length' activeInst.log` 
 
