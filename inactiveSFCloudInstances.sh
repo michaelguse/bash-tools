97f1a124d8a53ad27c746a0bc671fc23548869ab
Michael Guse <michael#optimizeconsulting.com>
2020-09-18 22:29:31 -0500

    Sorted the output

--- inactiveSFCloudInstances.sh
+++ inactiveSFCloudInstances.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -sS https://api.status.salesforce.com/v1/instances/ | jq -r '.[] | select(.isActive == false) | select(.releaseNumber == "") | [.isActive, .key, .releaseNumber, .status, .location] | @csv'
+curl -sS https://api.status.salesforce.com/v1/instances/ | jq -r 'sort_by(.location, .key) | .[] | select(.isActive == false) | select(.releaseNumber == "") | [.isActive, .key, .releaseNumber, .status, .location] | @csv'
