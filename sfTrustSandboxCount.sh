95df606f66389b8ca353105a0be751a665510cbb
Michael Guse <michael#optimizeconsulting.com>
2021-04-09 17:04:06 -0500

    Uopdated current and next release values.

--- sfTrustSandboxCount.sh
+++ sfTrustSandboxCount.sh
@@ -10,8 +10,8 @@ fi
 
 date "+%Y-%m-%d %H:%M:%S %Z" > newDate
 
-CURR_REL=$'Winter \'21'
-NEXT_REL=$'Spring \'21'
+CURR_REL=$'Spring \'21'
+NEXT_REL=$'Summer \'21'
 
 echo
 echo "|======================================================================|"
