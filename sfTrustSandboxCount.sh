ec70ee4454c1937e6fdd608ff362cb93680d4946
Michael Guse <guse.michael@gmail.com>
2023-02-14 14:10:59 -0600

    Updated curr and next release info in script

--- sfTrustSandboxCount.sh
+++ sfTrustSandboxCount.sh
@@ -10,8 +10,8 @@ fi
 
 date "+%Y-%m-%d %H:%M:%S %Z" > newDate
 
-CURR_REL=$'Winter \'23'
-NEXT_REL=$'Spring \'23'
+CURR_REL=$'Spring \'23'
+NEXT_REL=$'Summer \'23'
 
 echo
 echo "|======================================================================|"
