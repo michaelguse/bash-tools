9817ab72000cab97f82951beaf04b1a94f8abf72
Michael Guse <guse.michael@gmail.com>
2024-05-03 10:29:37 -0500

    Updated services by product script to improve comparison

--- sfTrustAllServicesbyProduct.sh
+++ sfTrustAllServicesbyProduct.sh
@@ -1,8 +1,8 @@
 #!/bin/zsh
 clear
 
-if test -f srv-prd-info.new; then
-  mv srv-prd-info.new srv-prd-info.old
+if test -f srv-prd-info_new.json; then
+  mv srv-prd-info_new.json srv-prd-info_old.json
 fi
 
 l_procDateTime=`date "+%Y-%m-%d %H:%M:%S %Z"`
@@ -20,7 +20,8 @@ echo '/--------------------------------------------------/'
 echo
 
 curl -sS -X GET "https://api.status.salesforce.com/v1/services" -H "accept: application/json" -o srv-prd-info.new
-
+jq 'map(. as $in | .Products[].key | . as $p | {product:$p, service:$in.key}) | group_by(.product) | map({product:.[0].product, services: map(.service)})' srv-prd-info.new > srv-prd-info_new.json
+  
 if test -s srv-prd-info.new; then
   echo "Service and Product Statistics:"
   echo "  # of unique SF products: `jq '.[]|.Products[]|.key' srv-prd-info.new |sort|uniq|wc -l`"
@@ -28,13 +29,14 @@ if test -s srv-prd-info.new; then
   echo
 fi
 
-diff srv-prd-info.old srv-prd-info.new > srvprd_hasChanged
+diff srv-prd-info_old.json srv-prd-info_new.json > srvprd_hasChanged
 
 if test -s srvprd_hasChanged; then
   echo "==> Detected changes since last run! <=="
   echo 
-  jq 'map(. as $in | .Products[].key | . as $p | {product:$p, service:$in.key}) | group_by(.product) | map({product:.[0].product, services: map(.service)})' srv-prd-info.new
-  echo
+  echo "CURRENT (NEW)                                                 ( PREVIOUS (OLD)                                 "
+  echo "--------------------------------------------------------------(------------------------------------------------"
+  diff -y srv-prd-info_new.json srv-prd-info_old.json
 else
   echo "No changes detected since last run!"
   echo
